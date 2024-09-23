6e89a370e6261f0f5fdd3158b76b5b763cf2ba89
Kiran George <gaurdian.kiran@gmail.com>
2024-09-23 22:15:12 +0200

    Randomized Upgrade Type

--- Assets/Scripts/Services/UI/UIService.cs
+++ Assets/Scripts/Services/UI/UIService.cs
@@ -22,8 +22,11 @@ public class UIService : MonoBehaviour
     private int currentMaxPlayerHealth;
     private int currentXpToNextLevel;
     private int currentPlayerLevel;
-    private int numberOfWeaponTypes = Enum.GetNames(typeof(WeaponType)).Length - 1;
-    private int numberOfUpgradeTypes = Enum.GetNames(typeof(UpgradeType)).Length - 1;
+    private int numberOfWeaponTypes = Enum.GetNames(typeof(WeaponType)).Length;
+    private int numberOfUpgradeTypes = Enum.GetNames(typeof(UpgradeType)).Length;
+
+    private int[] weapons;
+    private int[] upgrades;
     
     private Random random = new Random();
 
@@ -34,6 +37,18 @@ public class UIService : MonoBehaviour
         currentPlayerLevel = 1;
         pausePanel.SetActive(false);
         upgradesPanel.SetActive(false);
+        FillUpgradeCaches();
+    }
+
+    private void FillUpgradeCaches()
+    {
+        weapons = new int[numberOfWeaponTypes];
+        for (int i = 0; i < weapons.Length; i++)
+            weapons[i] = i;
+        
+        upgrades = new int[numberOfUpgradeTypes];
+        for (int i = 0; i < upgrades.Length; i++)
+            upgrades[i] = i;
     }
 
     private void Start()
@@ -105,16 +120,32 @@ public class UIService : MonoBehaviour
 
     private void GenerateUpgradesForPlayer()
     {
-        for(int i = 0; i < weaponToBeUpgradedTextList.Count; i++)
-            weaponToBeUpgradedTextList[i].text = GenerateRandomWeaponTypeForUpgrade().ToString();
-
-        for(int i = 0; i < upgradeTypeTextList.Count; i++)
-            upgradeTypeTextList[i].text = GenerateRandomUpgradeType().ToString();
+        FillUpgradeText<WeaponType>(weaponToBeUpgradedTextList, weapons);
+        FillUpgradeText<UpgradeType>(upgradeTypeTextList, upgrades);
     }
 
-    private WeaponType GenerateRandomWeaponTypeForUpgrade() =>
-        (WeaponType)random.Next(0, numberOfWeaponTypes);
-
-    private UpgradeType GenerateRandomUpgradeType() =>
-        (UpgradeType)random.Next(0, numberOfUpgradeTypes);
+    private void FillUpgradeText<T>(List<TextMeshProUGUI> textList, int[] dataArray) where T : Enum
+    {
+        ShuffleArray(dataArray);
+        int i = 0, j = 0;
+        for (; i < textList.Count && j < dataArray.Length; i++, j++)
+            textList[i].text = Enum.GetName(typeof(T), dataArray[j]);
+        
+        if(i < textList.Count - 1)
+            while (i < textList.Count)
+            {
+                textList[i].text = Enum.GetName(typeof(T), dataArray[j-1]);
+                i++;
+            }
+    }
+    
+    private void ShuffleArray(int[] array)
+    {
+        Random random = new Random();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+    }
 }
