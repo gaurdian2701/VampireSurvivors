b9688413910c81162b5324ae1c2abb4d7e3a5ac7
Kiran George <gaurdian.kiran@gmail.com>
2024-10-08 19:36:05 +0200

    Fixed the multishot

--- Assets/Scripts/Weapons/CrossbowController.cs
+++ Assets/Scripts/Weapons/CrossbowController.cs
@@ -9,29 +9,31 @@ public class CrossbowController : Weapon
     [SerializeField] private WeaponScriptableObject weaponData;
 
     private bool canShoot = true;
-    private int milliseconds = 1000;
-    private float positionalOffset = 0.25f;
+    private const int milliseconds = 1000;
+    private float degreeOffset = 30f;
     private void Awake()
     {
         InitWeaponData(weaponData);
         SubscribeToEvents();
     }
-
-    //DO THE THING YOU REMEMBERED THAT WORKED AND IT WAS COOL
+    
     public override void Attack()
     {
         if (!canShoot)
             return;
-        float angle = Mathf.Atan2(transform.up.y, transform.up.x);
-        for (int i = 0; i < 2; i++)
+        float offsetInRadians = Mathf.Deg2Rad * degreeOffset;
+        float middleAngle = Mathf.Atan2(transform.up.y, transform.up.x);
+        float startingAngle = middleAngle + offsetInRadians / 2;
+        float endingAngle = middleAngle - offsetInRadians / 2;
+        float currentAngle = startingAngle;
+        for (int i = 0; i < 4; i++)
         {
             ProjectileController projectileController =
                 GameManager.Instance.ObjectPoolingService.ProjectilePool.GetProjectileFromPool();
             projectileController.gameObject.SetActive(true);
             projectileController.InitDamageData(BaseDamage, BaseKnockBackForce);
-            float currentAngle = angle + positionalOffset;
-            positionalOffset *= -1f;
             Vector3 projectilePos = new Vector2(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle));
+            currentAngle += (startingAngle - endingAngle) / 4f;
             projectileController.transform.position = transform.position + projectilePos;
             projectileController.transform.up = (projectileController.transform.position - transform.position).normalized;
         }
